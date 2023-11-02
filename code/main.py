2499a21c550b82bcf6139edf1c7595c172249b7e
AshtonJohns <ashtonmjohns@gmail.com>
2023-11-02 10:26:48 -0500

    driver function implementation

--- code/main.py
+++ code/main.py
@@ -10,25 +10,27 @@ import random
 
 G=nx.Graph()
 
-G.add_node("A")
-G.add_node("B")
-G.add_node("C")
-G.add_node("D")
-G.add_node("E")
-G.add_node("F")
-G.add_node("G")
-G.add_node("H")
-G.add_node("I")
-G.add_node("J")
-G.add_node("K")
-G.add_node("L")
-G.add_node("M")
-G.add_node("N")
-G.add_node("O")
-G.add_node("P")
+# G.add_node("A")
+# G.add_node("B")
+# G.add_node("C")
+# G.add_node("D")
+# G.add_node("E")
+# G.add_node("F")
+# G.add_node("G")
+# G.add_node("H")
+# G.add_node("I")
+# G.add_node("J")
+# G.add_node("K")
+# G.add_node("L")
+# G.add_node("M")
+# G.add_node("N")
+# G.add_node("O")
+# G.add_node("P")
 
-G.add_edges_from()
+# G.add_edges_from()
+# G.add
 
+# undirected graph 1
 G.add_edge("A","B")
 G.add_edge("A","F")
 G.add_edge("A","E")
@@ -49,7 +51,7 @@ G.add_edge("K","L")
 G.add_edge("K","O")
 G.add_edge("L","P")
 
-
+G.nodes()
 
 pos={
     
@@ -76,3 +78,45 @@ nx.draw(G,pos=pos,with_labels=True,node_color="red",node_size=3000,font_color="w
 plt.margins(0.2)
 plt.show()
 
+
+# main driver
+def main():
+    #get task 1 undirected graph
+    
+    choice = True
+    showInvalidinput = False
+    createResults = False
+    while choice:
+        #get input and ask which task to view
+        print("Please select the task to be viewed:\n1. DFS & BFS on undirected graph\n2. \n3. \n4. Quit/Save Results")
+        opt1 = input("\nChoice: ")
+        if opt1.__eq__("1"):
+            print("\n\n___QUESTION___:\nStarting from any vertex, can DFS and BFS find all connected components of an undirected graph? ")
+        elif opt1.__eq__("4"):
+            print("Would you like to save these results?\n1. Yes\n2. No")
+            opt2 = input("\nChoice: ")
+            if opt2.__eq__("1"):
+                choice = False
+                createResults = True
+            elif opt2.__eq__("2"):
+                choice = False
+            else:
+                showInvalidinput = True
+        else:
+            showInvalidinput = True
+        # show when invalid input
+        if showInvalidinput:
+            print("\n\nPlease select a valid option\n\n")
+            choice = True
+            showInvalidinput = False
+            createResults = False
+
+    if createResults:
+        print("not implemnted ")
+        # do stuff here
+
+    print("\n\nThank you! Take care.")
+
+
+if __name__ == '__main__':
+    main()
