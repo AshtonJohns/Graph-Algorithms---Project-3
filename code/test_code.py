e8c97ffe6daf9108fe3f636a624684d0a1c86c32
AshtonJohns <ashtonmjohns@gmail.com>
2023-11-09 09:05:41 -0600

    updates

--- code/test_code.py
+++ code/test_code.py
@@ -66,131 +66,232 @@ import networkx as nx
 # Create a 2D NumPy array
 
 
-G=nx.DiGraph()
+# G=nx.DiGraph()
     
-task2graphedges = [(1,3),
-                    (3,2),
-                    (3,5),
-                    (2,1),
-                    (4,1),
-                    (4,2),
-                    (4,12),
-                    (5,6),
-                    (5,8),
-                    (6,8),
-                    (6,7),
-                    (8,9),
-                    (9,5),
-                    (9,11),
-                    (8,10),
-                    (10,9),
-                    (6,10),
-                    (7,10),
-                    (10,11),
-                    (11,12)]
-    #add edges
+# task2graphedges = [(1,3),
+#                     (3,2),
+#                     (3,5),
+#                     (2,1),
+#                     (4,1),
+#                     (4,2),
+#                     (4,12),
+#                     (5,6),
+#                     (5,8),
+#                     (6,8),
+#                     (6,7),
+#                     (8,9),
+#                     (9,5),
+#                     (9,11),
+#                     (8,10),
+#                     (10,9),
+#                     (6,10),
+#                     (7,10),
+#                     (10,11),
+#                     (11,12)]
+#     #add edges
 
-G.add_edges_from(task2graphedges)
+# G.add_edges_from(task2graphedges)
 
-# scc = max(nx.strongly_connected_components(G), key=len)
+# # scc = max(nx.strongly_connected_components(G), key=len)
 
-# print(scc)
+# # print(scc)
 
-scc = nx.strongly_connected_components(G)
+# scc = nx.strongly_connected_components(G)
 
-# #list comprehension 
-# temp_list = list(scc)
-# conn_comp = []
-# for connected_components_list in temp_list:
-#     conn_comp.append(list(connected_components_list))
+# # #list comprehension 
+# # temp_list = list(scc)
+# # conn_comp = []
+# # for connected_components_list in temp_list:
+# #     conn_comp.append(list(connected_components_list))
 
-# message1 = "Strongly Connected Components for the digraph: "
-# for scc_group in conn_comp:
-#     message1 += "\n"+ str(scc_group)
+# # message1 = "Strongly Connected Components for the digraph: "
+# # for scc_group in conn_comp:
+# #     message1 += "\n"+ str(scc_group)
 
-# print(message1)
-# #print(conn_comp)
-
-
-testter = nx.condensation(G)
-
-# nx.draw_networkx(testter,node_size=1000,width=3)
-# plt.margins(0.2)
-#plt.show()
-
-node_data = testter.nodes.data()
-map = {}
-for node_data_stuff in node_data:
-    test_str = str(node_data_stuff[1]['members'])
-    test_str = test_str[1:]
-    test_str = test_str[:-1]
-    print(test_str)
-    map[node_data_stuff[0]] = test_str
-    # print(node_data_stuff[0])
-    # print(node_data_stuff[1]['members']) 
-    #print(node_data_stuff)
-print(map)
-testter = nx.relabel_nodes(testter,map)
-#print(list(node_data))
-#edge_data = G.edges
-#print(edge_data)
-
-# nx.draw_networkx(testter,node_size=1000,width=3)
-# plt.margins(0.2)
-#plt.show()
-
-# edge_data = testter.edges
-# print(edge_data)
-#nx.spring_layout(testter)
-
-if nx.is_directed_acyclic_graph(testter):
-    print("true")
-
-testter = nx.dag_to_branching(testter)
-
-if nx.is_directed_acyclic_graph(testter):
-    print("true")
+# # print(message1)
+# # #print(conn_comp)
 
 
-node_data = testter.nodes.data()
-map = {}
-print(node_data)
-for node_data_stuff in node_data:
-    test_str = str(node_data_stuff[1]['source'])
-    # test_str = test_str[1:]
-    # test_str = test_str[:-1]
-    print(test_str)
-    map[node_data_stuff[0]] = test_str
-    # print(node_data_stuff[0])
-    # print(node_data_stuff[1]['members']) 
-    #print(node_data_stuff)
-print(map)
-testter = nx.relabel_nodes(testter,map)
+# testter = nx.condensation(G)
 
-node_data = testter.nodes.data()
-pos = {}
-print(node_data)
-i = 0
-for node_data_stuff in node_data:
-    #test_str = str(node_data_stuff[1]['source'])
-    # test_str = test_str[1:]
-    # test_str = test_str[:-1]
-    pos[node_data_stuff[0]] = (i,5)
-    i += 2.5
+# # nx.draw_networkx(testter,node_size=1000,width=3)
+# # plt.margins(0.2)
+# #plt.show()
+
+# node_data = testter.nodes.data()
+# map = {}
+# for node_data_stuff in node_data:
+#     test_str = str(node_data_stuff[1]['members'])
+#     test_str = test_str[1:]
+#     test_str = test_str[:-1]
+#     print(test_str)
+#     map[node_data_stuff[0]] = test_str
+#     # print(node_data_stuff[0])
+#     # print(node_data_stuff[1]['members']) 
+#     #print(node_data_stuff)
+# print(map)
+# testter = nx.relabel_nodes(testter,map)
+# #print(list(node_data))
+# #edge_data = G.edges
+# #print(edge_data)
+
+# # nx.draw_networkx(testter,node_size=1000,width=3)
+# # plt.margins(0.2)
+# #plt.show()
+
+# # edge_data = testter.edges
+# # print(edge_data)
+# #nx.spring_layout(testter)
+
+# if nx.is_directed_acyclic_graph(testter):
+#     print("true")
+
+# testter = nx.dag_to_branching(testter)
+
+# if nx.is_directed_acyclic_graph(testter):
+#     print("true")
 
 
-nx.draw(testter,pos=pos,node_size=700,with_labels=True,width=3,connectionstyle='arc3, rad=0.5')
+# node_data = testter.nodes.data()
+# map = {}
+# print(node_data)
+# for node_data_stuff in node_data:
+#     test_str = str(node_data_stuff[1]['source'])
+#     # test_str = test_str[1:]
+#     # test_str = test_str[:-1]
+#     print(test_str)
+#     map[node_data_stuff[0]] = test_str
+#     # print(node_data_stuff[0])
+#     # print(node_data_stuff[1]['members']) 
+#     #print(node_data_stuff)
+# print(map)
+# testter = nx.relabel_nodes(testter,map)
+
+# node_data = testter.nodes.data()
+# pos = {}
+# print(node_data)
+# i = 0
+# for node_data_stuff in node_data:
+#     #test_str = str(node_data_stuff[1]['source'])
+#     # test_str = test_str[1:]
+#     # test_str = test_str[:-1]
+#     pos[node_data_stuff[0]] = (i,5)
+#     i += 2.5
+
+
+G_weighted_undirected = nx.Graph()
+
+G_weighted_undirected.add_edge("A", "B",weight=22)
+G_weighted_undirected.add_edge("A","C",weight=9)
+G_weighted_undirected.add_edge("A","D",weight=12)
+G_weighted_undirected.add_edge("B","F",weight=36)
+G_weighted_undirected.add_edge("C","B",weight=35)
+G_weighted_undirected.add_edge("C","D",weight=4)
+G_weighted_undirected.add_edge("C","E",weight=65)
+G_weighted_undirected.add_edge("C","F",weight=42)
+G_weighted_undirected.add_edge("D","E",weight=33)
+G_weighted_undirected.add_edge("E","G",weight=23)
+G_weighted_undirected.add_edge("E","F",weight=18)
+G_weighted_undirected.add_edge("F","G",weight=39)
+G_weighted_undirected.add_edge("B","H",weight=34)
+G_weighted_undirected.add_edge("D","I",weight=30)
+G_weighted_undirected.add_edge("H","I",weight=19)
+G_weighted_undirected.add_edge("F","H",weight=24)
+G_weighted_undirected.add_edge("G","I",weight=21)
+G_weighted_undirected.add_edge("G","H",weight=25)
+
+
+pos={ # positions for nodes
+    
+    "A":(0,7.5),
+    "B":(2.5,10),
+    "C":(2.5,7.5),
+    "D":(2.5,2.5),
+    "E":(5,5),
+    "F":(5,7.5),
+    "G":(7.5,5),
+    "H":(10,10),
+    "I":(10,2.5),
+}
+# start_node = "A"
+# nodes = G_weighted_undirected.nodes()
+# spt = nx.single_source_dijkstra_path(G_weighted_undirected,start_node,weight='weight')
+# message = "Nodes: \n" + str(nodes) + "\nDijkstra's algorithm found the shortest path tree:\n"
+# for x,value in spt.items():
+#     message += "Shortest path from " + start_node + " to " + x + ":\n"
+#     message += str(value) + "\n"
+
+# print(message)
+
+#mst = nx.minimum_spanning_tree(G_weighted_undirected,weight='weight',algorithm="kruskal")
+
+#mstPath = nx.minimum_spanning_edges(G_weighted_undirected, algorithm="kruskal",)
+
+#mstPath = nx.minimum_branching(G_weighted_undirected,attr='weight')
+# edgelist = list(mstPath)
+
+nodes = G_weighted_undirected.nodes()
+
+spt = nx.shortest_path(G_weighted_undirected,weight='weight',method="dijkstra")
+
+spt = nx.shortest
+
+# dms = nx.multi_source_dijkstra(G_weighted_undirected,weight='weight',sources=nodes)
+
+mst = nx.minimum_spanning_tree(G_weighted_undirected,weight='weight',algorithm='kruskal')
+
+mstEdges = mst.edges()
+mstNodes = mst.nodes.data()
+
+
+print(mstEdges)
+print(mstNodes)
+
+# print(spt)
+# print(edgelist)
+
+print("\nspt\n")
+print(type(spt))
+for x,value in spt.items():
+    print(x)
+    print(value)
+
+# print("\ndms\n")
+# print(type(dms))
+# for x,value in dms.items():
+#     print(x)
+#     print(value)
+
+# print("mst")
+# print(mstPath)
+# for x in mstPath:
+#     print(x)
+
+# print(dms)
+
+test_G = nx.Graph()
+#test_G.add_edges_from(mstPath)
+
+test_G2 = nx.Graph()
+
+# test_G2.add_edges_from(dms)
+
+
+
+# nx.draw(test_G2,pos=pos,node_size=700,with_labels=True,width=3)
+# plt.show()
 
 
 
 
 
 
-#plt.margins(0.2)
-plt.show()
+
 # # nx.spreadout(G)
 # # plt.margins(1,1)
 # # figure = plt.gcf()
 # # figure.set_size_inches(5,5)
 # plt.savefig("testter.png")
 
+# nx.draw(spt,pos=pos,node_size=700,with_labels=True,width=3)
+# plt.show()
